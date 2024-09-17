a08364b95dba7ae8f7c00feed629914f62434009
rcobb@colgate.edu <rcobb@colgate.edu>
2024-09-17 02:09:27 +0000

    Created ServerPackage1; Edited ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -11,12 +11,14 @@ class Form1(Form1Template):
   
   def text_box_1_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
-    compName, compID, compSN, compAsset, prestageID, prestageName = anvil.server.call('get_target_computer', self.text_box_1.text)
+    #compName, compID, compSN, compAsset, prestageID, prestageName = anvil.server.call('get_target_computer', self.text_box_1.text)
+    tokentest = anvil.server.call('get_target_computer', self.text_box_1.text)
+    self.cName.text = f"{tokentest}"
     
-    self.cName.text = f"{compName}"
-    self.cSN.text = f"{compSN}"
-    self.cAsset.text = f"{compAsset}"
-    self.cID.text = f"{compID}"
-    self.pID.text = f"{prestageID}"
-    self.pName.text = f"{prestageName}"
+    #self.cName.text = f"{compName}"
+    #self.cSN.text = f"{compSN}"
+    #self.cAsset.text = f"{compAsset}"
+    #self.cID.text = f"{compID}"
+    #self.pID.text = f"{prestageID}"
+    #self.pName.text = f"{prestageName}"
     
