b8197a01705448ad8dc69ff70ccde95c961be274
rcobb@colgate.edu <rcobb@colgate.edu>
2024-11-20 15:23:44 +0000

    Edited settings, ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,11 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
 import anvil.server
 
 
@@ -31,6 +37,7 @@ class Form1(Form1Template):
     
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
+    anvil.google.auth.login()
   
   def text_box_1_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
