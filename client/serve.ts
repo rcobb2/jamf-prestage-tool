2428bc3e05502aabeddc1e2c7ecbdab90848380f
CatGamer23 <lfiller23@gmail.com>
2025-06-05 08:34:30 -0400

    Fix script reference in index.html and update server configuration in serve.ts

--- client/serve.ts
+++ client/serve.ts
@@ -3,6 +3,7 @@ import homepage from "/app/client/index.html"
 const server: Bun.Server = Bun.serve({
   development: {
     console: true,
+    hmr: false,
   },
   hostname: process.env.CLIENT_API_HOSTNAME || "localhost",
   port: process.env.CLIENT_API_PORT || 3000,
