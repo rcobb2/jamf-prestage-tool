c99d768769c899ae23d0d013d3953725f6541a31
CatGamer23 <lfiller23@gmail.com>
2025-06-13 09:15:32 -0400

    Add custom 404 page and update server routing to handle not found errors; also moved serve.ts inside worker.ts

--- client/worker.ts
+++ client/worker.ts
@@ -1,5 +1,8 @@
+import notFound from "/app/404.html";
+import homepage from "/app/client/index.html";
+
 await Bun.build({
-  entrypoints: [ './main.ts' ],
+  entrypoints: ['./main.ts'],
   outdir: './client/',
   env: 'inline',
   target: 'browser',
@@ -11,4 +14,18 @@ await Bun.build({
 
 console.log('Client build completed successfully.');
 
-await import('./serve.ts');
+const server: Bun.Server = Bun.serve({
+  development: false,
+  hostname: process.env.CLIENT_HOSTNAME || "localhost",
+  port: process.env.CLIENT_PORT || 3000,
+  tls: {
+    key: Bun.file("server.key"),
+    cert: Bun.file("server.cert"),
+  },
+  routes: {
+    "/": homepage,
+    "/*": notFound,
+  },
+});
+
+console.log(`Client listening on ${server.url}`);
